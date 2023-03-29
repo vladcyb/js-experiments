dbbb2a45ef3e078cc3265995978395172ecd45b2
Vlad Tsibizov <vladtsibizov@yandex.ru>
2023-03-30 01:13:53 +0300

    fix

--- src/has.ts
+++ src/has.ts
@@ -4,15 +4,15 @@
  * @param path
  * @returns true, если существует, false в противном случае
  */
-export function has (obj: unknown, path: string) {
+export function has (obj: any, path: string) {
   const keys = path.replace(/\]$/, '').split(/\.|\[|\]\.|\]\[|\]/)
-  let currentObj: unknown = obj
+  let currentObj = obj
 
   for(const key of keys) {
     if (currentObj == null || !Object.hasOwn(currentObj, key)) {
       return false
     }
-    currentObj = (currentObj as Record<string, unknown>)[key]
+    currentObj = currentObj[key]
   }
 
   return true
